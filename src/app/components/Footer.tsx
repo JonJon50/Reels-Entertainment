9c50d703ec8291201f725562e82366007ee062fd
John <ginuwine104@gmail.com>
2025-02-17 10:41:33 -0500

    Footer error fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaInstagram, FaTiktok, FaYoutube, FaTwitch } from "react-icons/fa";
+import { FaInstagram, FaTiktok, FaTwitch } from "react-icons/fa";
 
 export default function Footer() {
     return (
